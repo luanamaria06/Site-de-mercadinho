1a4f3ec7b855fad338375ef2e603338ecd7e826f
Matheus Ramalho de Lima <101842192+pitoko01@users.noreply.github.com>
2024-05-15 16:13:35 -0300

    Carrossel e pasta do JS
    Fiz o carrossel, só falta colocar ele na posição certa e colocar algumas mudanças ainda nele.

---
+++ script.js
@@ -0,0 +1,40 @@
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
+
+document.querySelector('.search-bar button').addEventListener('click', function() {
+    alert('Sem itens no catalogo tente novamente mais tarde :D'); // Esta é uma mensagem de exemplo. Substitua pela funcionalidade real.
+});
