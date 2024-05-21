3bf76e6c86242acca61dd2c801296558bd895a93
Matheus Ramalho de Lima <101842192+pitoko01@users.noreply.github.com>
2024-05-21 08:43:02 -0300

    alou

--- script.js
+++ script.js
@@ -58,28 +58,3 @@ const prevSlider2 = () => {
 btnNext2.addEventListener('click', nextSlider2);
 btnPrev2.addEventListener('click', prevSlider2);
 
-function signup() {
-  const fullName = document.getElementById('fullName').value;
-  const email = document.getElementById('email').value;
-  const phone = document.getElementById('phone').value;
-  const cpf = document.getElementById('cpf').value;
-  
-  if (fullName && email && phone && cpf) {
-      document.querySelector('.signup-container').style.display = 'none';
-      document.querySelector('.loading-container').style.display = 'flex';
-      
-      setTimeout(() => {
-          alert(`Cadastro realizado! Boas compras`);
-          window.location.href = "site_supemercado.html"; 
-      }, 3000); // Simula um carregamento de 3 segundos
-  } else {
-      alert('Por favor, preencha todos os campos.');
-  }
-
-
-function login() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    alert(`Login com email: ${email} e senha: ${password}`);
-  }
-}
