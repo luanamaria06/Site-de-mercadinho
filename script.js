a57993749f931e8863b58248d8b26863974db59b
Matheus Ramalho de Lima <101842192+pitoko01@users.noreply.github.com>
2024-05-21 07:16:45 -0300

    alguns ajustes
    só isso

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
 const slider = document.querySelectorAll('.slider');
 const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
-const searchBarButton = document.querySelector('.search-bar button'); // Armazenar o botão da barra de pesquisa
+const searchBarButton = document.querySelector('.search-bar button'); 
 
 let currentSlide = 0;
 
-// Esconde todos os slides
+
 const hideSlider = () => slider.forEach(item => item.classList.remove('on'));
 
-// Mostra o slide atual
 const showSlider = () => {
   slider[currentSlide].classList.add('on');
 };
 
-// Avança para o próximo slide
 const nextSlider = () => {
   hideSlider();
   currentSlide = (currentSlide + 1) % slider.length; // Simplifica a lógica para avançar o slide
   showSlider();
 };
 
-// Retorna ao slide anterior
 const prevSlider = () => {
   hideSlider();
-  currentSlide = (currentSlide - 1 + slider.length) % slider.length; // Simplifica a lógica para retornar o slide
+  currentSlide = (currentSlide - 1 + slider.length) % slider.length; 
   showSlider();
 };
 
 btnNext.addEventListener('click', nextSlider);
 btnPrev.addEventListener('click', prevSlider);
 
-// Adiciona funcionalidade ao botão da barra de pesquisa
 searchBarButton.addEventListener('click', () => {
     alert('Sem itens no catálogo. Tente novamente mais tarde :D'); // Mensagem de placeholder
 });
 
-// Novas linhas para o segundo carrossel
-const slider2 = document.querySelectorAll('.slider-2'); // Use a classe correta para os slides do segundo carrossel
+//segundo carrossel
+const slider2 = document.querySelectorAll('.slider-2'); 
 const btnPrev2 = document.getElementById('prev-button-2');
 const btnNext2 = document.getElementById('next-button-2');
 
